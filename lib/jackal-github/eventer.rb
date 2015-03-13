3f3c824fbccf5b1656102a743f312bb0e163d7d6
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-13 16:37:03 -0700

    Provide filtering ability based on received event type and settings defined within configuration and URL parameters.

--- lib/jackal-github/eventer.rb
+++ lib/jackal-github/eventer.rb
@@ -22,8 +22,58 @@ module Jackal
         failure_wrap(message) do |_|
           content = format_payload(message)
           payload = new_payload(name_for_payload(content), content)
-          job_completed(:github, payload, message)
+          filter(message, payload) do
+            job_completed(:github, payload, message)
+          end
         end
+      end
+
+      # Apply filters to payload. If filters are enabled
+      # and no filter matches, discard payload
+      #
+      # @param message [Carnivore::Message]
+      # @param payload [Smash]
+      # @yield block to execute if payload is allowed
+      def filter(message, payload)
+        valid = true
+        a_events = allowed_events(payload)
+        r_events = restricted_events(payload)
+        if(a_events && !a_events.include?(payload.get(:data, :github, :event)))
+          valid = false
+        end
+        if(r_events && r_events.include?(payload.get(:data, :github, :event)))
+          valid = false
+        end
+        if(valid)
+          yield
+        else
+          warn "Message has been filtered and is restricted from entering pipeline (#{message})"
+          message.confirm!
+        end
+      end
+
+      # Event types allowed to be dropped into pipeline
+      #
+      # @param payload [Smash]
+      # @return [Array, NilClass]
+      def allowed_events(payload)
+        a_events = config.fetch(:events, :allowed, [])
+        a_events += payload.fetch(:data, :github, :query, {}).map do |k,v|
+          k if v == 'enabled'
+        end.compact
+        a_events.empty? ? nil : a_events
+      end
+
+      # Event types restricted from being dropped into pipeline
+      #
+      # @param payload [Smash]
+      # @return [Array, NilClass]
+      def restricted_events(payload)
+        r_events = config.fetch(:events, :restricted, [])
+        r_events += payload.fetch(:data, :github, :query, {}).map do |k,v|
+          k if v == 'disabled'
+        end.compact
+        r_events.empty? ? nil : a_events
       end
 
       # Format the github event to store within the payload
