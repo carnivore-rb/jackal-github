e562df0d8432b527e786ab87365add3111710bb9
Chris Roberts <code@chrisroberts.org>
2015-06-10 07:10:55 -0700

    Add service registration information

--- lib/jackal-github.rb
+++ lib/jackal-github.rb
@@ -8,3 +8,23 @@ end
 
 require 'jackal-github/formatter'
 require 'jackal-github/version'
+
+Jackal.service(
+  :github,
+  :description => 'Accept GitHub event payloads',
+  :category => :modifier,
+  :configuration => {
+    :names => {
+      :type => :hash,
+      :description => 'Set payload name based on event type'
+    },
+    :events__allowed => {
+      :type => :array,
+      :description => 'List of allowed event types'
+    },
+    :events__restricted => {
+      :type => :array,
+      :description => 'List of restricted event types'
+    }
+  }
+)
